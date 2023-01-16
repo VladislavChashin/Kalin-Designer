7c04b9aeae5c4276167474f7286051cf7bca96fb
vladislav <chasinvlad39@gmail.com>
2023-01-16 22:31:09 +0300

    anim

--- script/app.js
+++ script/app.js
@@ -1,50 +1,15 @@
-var znach = {
-    info_metrop:{
-        address: 'Адрес: Ленинградское шоссе, 16Ас1',
-        conversion: '11.3%',
-        passability: '20.000',
-        square: '855.5м',
-    },
-    info_europes:{
-        address: 'Адрес: площадь Киевского вокзала, 2',
-        conversion: '12.5%',
-        passability: '24.000',
-        square: '1185м',
-    },
-    info_pavlec:{
-        address: 'Адрес: Павелецкая площадь, 3',
-        conversion: '10%',
-        passability: '7.300',
-        square: '258.8',
-    },
-    info_europolis:{
-        address: 'Адрес: проспект Мира, 211, корп. 2',
-        conversion: '10.4%',
-        passability: '9.000',
-        square: '285.7м',
-    },
-    info_vesna:{
-        address: 'Адрес: Алтуфьевское шоссе, 1-й километр',
-        conversion: '10.8%',
-        passability: '8.300',
-        square: '215.9м',
-    },
-    info_afimol:{
-        address: 'Адрес: Пресненская набережная., 2',
-        conversion: '10.5%',
-        passability: '4.936',
-        square: '514.8м',
-    },
-}
+const anchors = document.querySelectorAll('a[href^="#"]')
 
-var app_nav1 = new Vue({
-    el: "#app_nav-1",
-    data: {
-            message_adres: 'Адрес: Ленинградское шоссе, 16Ас1',
-            message_convers: '11.3%',
-            message_passability: '20.000',
-            message_square: '855.5м',
-            vasa: 'фывфывф',
-        },  
-    },
-})
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
